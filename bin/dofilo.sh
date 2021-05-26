211997576a23d6a8ce77e4ce58512974423642b2
Jibundeyare <contact@jibundeyare.com>
2021-05-26 15:08:32 +0200

    Ajout de code

--- bin/dofilo.sh
+++ bin/dofilo.sh
@@ -6,3 +6,5 @@ php bin/console doctrine:schema:drop --full-database --force --no-interaction
 php bin/console doctrine:migrations:migrate --no-interaction
 # validation du schéma de BDD
 php bin/console doctrine:schema:validate
+# injection des données de test dans la BDD
+php bin/console doctrine:fixtures:load --no-interaction
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('foo@example.com');
+        $user->setPassword('123');
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
