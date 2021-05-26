ef8156e4ec027de494037f4aaa3344f8e9872131
Jibundeyare <contact@jibundeyare.com>
2021-05-26 17:06:23 +0200

    Ajout de code

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,19 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Student;
 use App\Entity\User;
+use App\Entity\SchoolYear;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+    private $faker;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+        $this->faker = \Faker\Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager)
     {
+        $schoolYear = new SchoolYear();
+        $schoolYear->setName($this->faker->name());
+        $schoolYear->setStartDate($this->faker->dateTimeThisDecade());
+        // récupération de la date de début
+        $startDate = $schoolYear->getStartDate();
+        // ajout d'un interval de 4 mois à la date de début
+        $endDate = $startDate->add(new \DateInterval('P4M'));
+        $schoolYear->setEndDate($endDate);
+
+        $manager->persist($schoolYear);
+
         $user = new User();
-        $user->setEmail('foo@example.com');
-        $user->setPassword('123');
+        $user->setEmail($this->faker->email());
+        // hashage du mot de passe
+        $password = $this->encoder->encodePassword($user, '123');
+        $user->setPassword($password);
+        $user->setRoles(['ROLE_STUDENT']);
 
         $manager->persist($user);
+
+        $student = new Student();
+        $student->setFirstname($this->faker->firstname());
+        $student->setLastname($this->faker->lastname());
+        $student->setPhone($this->faker->phoneNumber());
+        $student->setSchoolYear($schoolYear);
+        $student->setUser($user);
+
+        $manager->persist($student);
 
         $manager->flush();
     }
